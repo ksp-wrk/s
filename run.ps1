ec65605609271e09805a24c3c0fd5f77516a71d1
ksp-wrk <ksp.wrk@gmail.com>
2025-05-31 06:24:16 -0700

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -31,8 +31,8 @@ if (!(Get-Command curl -ErrorAction SilentlyContinue)) {
   Write-Host "curl is already installed."
 }
 
-$WBPV = "D:\WBPV.exe"
-$WPs = "D:\out.html"
+$WBPV = "C:\WBPV.exe"
+$WPs = "C:\out.html"
 
 if (!(Test-Path $WBPV)) {
   # Download curl
